a502b842cd782cffe60a8b16417b58919ac59f10
critsmet <36425627+critsmet@users.noreply.github.com>
2018-06-07 18:37:25 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+require "Pry"
+
+class Project
+
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title=title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    self.backers << backer
+    backer.backed_projects << self
+  end
+end
